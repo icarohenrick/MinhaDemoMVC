2f8e2bf96eabf0b5d56d0d9f8d595e2c08d17530
Icaro Henrique <icaro_henrick@hotmail.com>
2020-06-30 11:16:04 -0300

    Adicionando Projeto MVC Básico em dotNetCore 3.1 para o Git

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MinhaDemoMVC.Models;
+
+namespace MinhaDemoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(string id, string categoria)
+        {
+            var filme = new Filme
+            {
+                Titulo = "Piratas do Caribe",
+                DataLancamento = DateTime.Now,
+                Genero = null,
+                Avaliacao = 10,
+                Valor = 20000
+            };
+            //return RedirectToAction("Privacy", filme);
+            return View();
+        }
+
+        public IActionResult Privacy(Filme filme)
+        {
+            if(ModelState.IsValid)
+            {
+
+            }
+
+            foreach(var erros in ModelState.Values.SelectMany(m => m.Errors))
+            {
+                Console.WriteLine(erros.ErrorMessage);
+            }
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Filme.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MinhaDemoMVC.Models
+{
+    public class Filme
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Título é obrigatório")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage ="O Título precisa ter entre 3 e 60 caracteres")]
+        public string Titulo { get; set; }
+
+        [DataType(DataType.DateTime, ErrorMessage = "Data em formato incorreto")]
+        [Required(ErrorMessage = "O Campo de lançamento é obrigatório")]
+        [Display(Name = "Data de Lançamento")]
+        public DateTime DataLancamento { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\u00C0-\u00FF""'\w-]*$", ErrorMessage ="Gênero em formato errado")]
+        [StringLength(30, ErrorMessage = "Máximo de 30 caracteres"), Required(ErrorMessage = "O campo gênero é requerido")]
+        [Display(Name = "Gênero")]
+        public string Genero { get; set; }
+
+        [Range(1, 1000, ErrorMessage = "Valor de 1 a 1000")]
+        [Required(ErrorMessage = "Preencha o campo Valor")]
+        [Column(TypeName = "decimal(18,2")]
+        public decimal Valor { get; set; }
+
+        [RegularExpression(@"^[0-5]*$", ErrorMessage = "Somente números")]
+        [Required(ErrorMessage = "Preencha o campo Avaliação")]
+        [Display(Name = "Avaliação")]
+        public int Avaliacao { get; set; }
+    }
+}
